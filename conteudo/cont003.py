b134174109d135dd97bbe0e68a87a86cd49acee0
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-03-09 08:57:02 -0300

    Create cont003.py

---
+++ conteudo/cont003.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric()) #para perguntar se é um número
+print(n.isalpha()) #para perguntar se é letra
+print(n.isalnum()) #para perguntar se é letra ou número
+print(n.isupper()) #para perguntar se é so com letras maiusculas
+print(n.isdecimal()) #para verificar se é decimal
+print(n.isdigit()) #para verificar se é um digito
+print(n.islower()) #para verificar se é apenas letras minusculas
+print(n.isidentifier()) #para verificar se é um identificador válido
+print(n.isspace()) #para verificar se tem espaço em branco
