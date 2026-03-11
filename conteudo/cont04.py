afcf71a738e41585d03c014c23934bc2d41b854e
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-03-11 11:40:46 -0300

    Create cont04.py

---
+++ conteudo/cont04.py
@@ -0,0 +1,7 @@
+# O + é utilizado como operador de adição
+# O - é utilizado como sinal de subtração
+# O * é utilizado parafazer a multiplicação
+# O / é utilizada para fazer divisão
+# O ** é utilizado para fazer a exponenciação
+# O // é utilizado para fazer a divisão inteira
+# O % é utilzada para o resto da divisão#
