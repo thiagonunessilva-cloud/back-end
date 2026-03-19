7be66a8414d2c2f8c1cb4d288cf97bebab2920a2
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-03-19 07:50:20 -0300

    Create ex006.py

---
+++ exercício/ex006.py
@@ -0,0 +1,3 @@
+# faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+n1= int(input('um valor'))
+print('o número digitado foi{} sendo o seu sucessor {} e seu antecessor {}'.format (n1, (n1+1), (n1-1)))
