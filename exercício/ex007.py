b22bce2265e9273c60d52c89305d7f26e628a9cd
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-03-19 08:10:02 -0300

    Update ex007.py

--- exercício/ex007.py
+++ exercício/ex007.py
@@ -1,3 +1,3 @@
 n1=int(input('digite um numero'))
-print('o numero digitado foi{}o seu dobro é{}, seu triplo{} e ua raiz quadradae{:.2f}'.format(n1,n1*2,n1*3,n1*0,5))
+print('o numero digitado foi{}o seu dobro é{}, seu triplo{} e ua raiz quadradae{:.2f}'.format(n1,n1*2,n1*3,n1**0,5))
 # para calcular a raiz posso ultilizara fução pow(n**0.5)
