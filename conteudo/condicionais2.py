03adf1631d2a7aff2c0d69bc760af8e90962d303
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-05-11 10:55:56 -0300

    Create condicionais2.py

---
+++ conteudo/condicionais2.py
@@ -0,0 +1,6 @@
+#Condições
+# Em condicionais devemos deixar o código indentado, ou seja, com o espaçamento correto demonstrando qual bloco de código pertence ao operador condicional. 
+# Para realizar a indentração podemos utilizar a tecla tab. 
+# Em python para o se utilizamos if+confição+:, já o senão utilizamos o else + :,
+# depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados.
+# Nesse tipo de condicional apenas um bloco é executado.
