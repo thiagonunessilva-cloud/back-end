fc76a3bb53a275ef7621bea4b2ca7b189991ae53
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-05-11 10:52:23 -0300

    Create condicionais1.py

---
+++ condicionais1.py
@@ -0,0 +1,7 @@
+#CONDIÇÕES
+#estrutura senquencial: Os comandos condicionais são executados de cima para baixo em um bloco de códigos.  
+#  #Até es#se momento estávamos trabalhando nessa lógica, porém nas estruturas condicionais temos duas possibilidade de caminhos. 
+# Cada caminho terá um bloco de comandoOs comandos condicionais são executados de cima para baixo em um bloco de códigos.
+# Até esse momento estávamos trabalhando nessa lógica, porém nas estruturas condicionais temos duas possibilidades de caminhos.    
+# Cada caminho terá um bloco de comando executados se ele for escolhido. Em programação utilizamos o comando se, se alguma condição verdadeira executa um bloco de códigos, 
+# e podemos utilizar um bloco se não para executar quando a condição for falsa.
