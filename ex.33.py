7d55c65f2ec86cffab11e72760d276056c1c0e91
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-05-20 10:49:50 -0300

    Create ex.33.py

---
+++ ex.33.py
@@ -0,0 +1,9 @@
+salario = float(input("Qual é o salário do funcionário?"))
+aumento15 = salario + (salario*0.15)
+aumento10 salario + (salario*0.10)
+if salario > 1250:
+print("Quem ganhava R${} passa a ganhar R${}".
+format(salario, aumento10))
+else:
+print("Quem ganhava R${) passa a ganhar R${}".
+format(salario, aumento15))
