cdfa4b415b85f988a127a2e12858781e56356ffb
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-06-25 09:46:25 -0300

    Create ex40.py

---
+++ ex40.py
@@ -0,0 +1,14 @@
+print("-=-" * 10)
+print("Analisador de triângulos")
+print("-=-" * 10)
+a = float(input("Primeiro segmento"))
+b = float(input("Segundo segmento "))
+c = float(input("Terceiro segmento"))
+c: f a<b+c and b<a+c and c<a+b and a== b ==
+print("Os segmentos acima podem formar um triangulo Equilatero")
+elif a<b+c and b<a+c and c<a+b and a== b or a== c or b==c:
+  print("Os segmentos acima podem formar um triangulo Isósceles")
+elif a<b+c and b<a+c and c<a+b and a!= b and a!= c and b!=c:
+  print("Os segmentos acima podem formar um triangulo Escaleno")
+else:
+  print("Não é possível formar um triãngulo.")
