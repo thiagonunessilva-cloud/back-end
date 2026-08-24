170c5af564f8c30eacc632869980a54f5b77b66e
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-08-24 10:45:56 -0300

    Create ex.58.py

---
+++ exercício/ex.58.py
@@ -0,0 +1,11 @@
+print('Gerador de PA')
+print('--'*10)
+primeiro = int(input('primeiro termo:'))
+razao = int(input('razão d 'razão da PA:'))
+termo = primeiro
+cont = 1
+while cont <=10:
+ print(f'{termo} ->', end='')
+ termo += razao
+ cont +=1
+print('fim')
