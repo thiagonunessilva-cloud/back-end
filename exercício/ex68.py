0dd1aa9c5520fcde0e4f4f031ab873b9bbd4da7a
thiagonunessilva-cloud <thiago.nunes.silva@escola.pr.gov.br>
2026-09-14 11:32:39 -0300

    Create ex68.py

---
+++ exercício/ex68.py
@@ -0,0 +1,24 @@
+print('='*30)
+print('BANCO CEV')
+print('='*30)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if total>= ced:
+        total -= ced
+        totalced += 1
+    else:
+        print(f'Total de {totalced} cédulas de R${ced}')
+    if ced == 50:
+        ced = 20
+    elif ced == 20:
+        ced = 10
+    elif ced == 10:
+        ced = 1
+    totalced = 0
+    if total ==0:
+        break
+print('='*30)
+print('VOLTE SEMPRE')
